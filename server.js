c09e46ad5750589575b71fd415d53e933322ce60
Tom Burnett <t_burnett@outlook.coml>
2017-01-12 18:02:41 +0000

    Prep server for Heroku

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
+app.use(function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] === 'http') {
+        next();
+    } else {
+        res.redirect('http://' + req.hostname + req.url);
+    }
+});
+
 app.use(express.static('public'));
 
-app.listen(3000, () => {
-    console.log('Server started on port 3000');
+app.listen(PORT, () => {
+    console.log(`Server started on ${PORT}`);
 });
